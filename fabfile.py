c74df4d1cdc80def613a87c195855c7e4a388774
Samuel Fuentes <info@samuelfuentes.com>
2014-08-08 22:58:51 +0100

    Fix fabric

--- fabfile.py
+++ fabfile.py
@@ -88,8 +88,8 @@ def deploy():
     pull()
     install_requirements()
     with cd(project_name):
-        run("python manage.py syncdb")
-        run("python manage.py migrate")
+        run("env/bin/python manage.py syncdb")
+        run("env/bin/python manage.py migrate")
     reload_services()
 
 @task
